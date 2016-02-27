bb4670ed7e2fc05de7f975e85fb9d5ff422c14ce
JIAO Jie <thomasjiao@vip.qq.com>
2016-02-27 10:14:07 +0800

    fix feof() warning, according to its git repo ISSUE#4 @Sat Feb 27 10:14:07 CST 2016

--- vendor/react/socket/Connection.php
+++ vendor/react/socket/Connection.php
@@ -4,10 +4,9 @@ namespace React\Socket;
 
 use React\Stream\Stream;
 
-class Connection extends Stream implements ConnectionInterface
-{
-    public function handleData($stream)
-    {
+class Connection extends Stream implements ConnectionInterface {
+
+    public function handleData($stream) {
         // Socket is raw, not using fread as it's interceptable by filters
         // See issues #192, #209, and #240
         $data = stream_socket_recvfrom($stream, $this->bufferSize);
@@ -15,13 +14,12 @@ class Connection extends Stream implements ConnectionInterface
             $this->emit('data', array($data, $this));
         }
 
-        if ('' === $data || false === $data || feof($stream)) {
+        if ('' === $data || false === $data || !is_resource($stream) || feof($stream)) {
             $this->end();
         }
     }
 
-    public function handleClose()
-    {
+    public function handleClose() {
         if (is_resource($this->stream)) {
             // http://chat.stackoverflow.com/transcript/message/7727858#7727858
             stream_socket_shutdown($this->stream, STREAM_SHUT_RDWR);
@@ -30,13 +28,12 @@ class Connection extends Stream implements ConnectionInterface
         }
     }
 
-    public function getRemoteAddress()
-    {
+    public function getRemoteAddress() {
         return $this->parseAddress(stream_socket_get_name($this->stream, true));
     }
 
-    private function parseAddress($address)
-    {
+    private function parseAddress($address) {
         return trim(substr($address, 0, strrpos($address, ':')), '[]');
     }
+
 }
